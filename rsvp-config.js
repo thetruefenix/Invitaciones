f7bff3b7b29ca4a756006f45222a6c9aae65be88
thetruefenix <thetruefenix@gmail.com>
2026-04-22 13:41:22 -0400

    rv

--- rsvp-config.js
+++ rsvp-config.js
@@ -1,3 +1,3 @@
 window.RSVP_CONFIG = {
-  endpoint: "https://script.google.com/macros/s/AKfycbxd4n0dx8iP1VHRQm9IN9xND6eLTHpwhZnvyXZ95H3sn8RKEReyyz-6ub_yZPBc1NFg5Q/exec",
+  endpoint: "https://script.google.com/macros/s/AKfycbys23HP_TQR-v4RDPdi9ZLZPFGeG1-wxtvLuf5jaOwhOhTp4hP4O2VVipIFFOCgsw1U/exec",
 };
